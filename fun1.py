3f35a048369ce47e4b0b6de9e264137fac96f39c
william19-meet <william19@meet.mit.edu>
2017-07-30 11:51:27 +0300

    Stopping here

--- fun1.py
+++ fun1.py
@@ -1,8 +1,11 @@
-def add_numbers():
+def add_numbers(x , z):
     c = 0
-    for num in range(1 , 100+ 1):
+    for num in range(x , z+ 1):
         print(num)
         c = c + num
     return c
-answer = add_numbers()
+
+
+
+answer = add_numbers(333 , 777)
 print(answer)
---
+++ fun2.py
@@ -0,0 +1,11 @@
+def draw_1d(n , char):
+    c1 = '!'
+    c2 = '^'
+    print('char' *n)
+    return n
+answer = draw_1d
+print(answer)
+    
+    
+
+
